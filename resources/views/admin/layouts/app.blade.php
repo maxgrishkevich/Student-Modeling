7d03235c2b7d087f5018a757657d43907e44c2aa
maxgrishkevich <maksim2002gri@gmail.com>
2023-05-29 01:19:26 +0300

    code clean up

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <script src="{{ asset('js/admin/plugins/chartjs.min.js') }}"></script>
 </head>
 
-<body class="g-sidenav-show   bg-gray-100">
+<body class="g-sidenav-show bg-gray-100">
 <div class="min-height-300 bg-primary position-absolute w-100"></div>
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
